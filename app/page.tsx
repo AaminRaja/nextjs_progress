262984e244d67b08a944a3b0265c8daa8cc69f69
Aamin Raja <be.intern02@appadore.com>
2024-12-20 11:03:53 +0530

    Eslint & Prettier setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
-import Image from "next/image";
-
-let Home = () => {
-  return (
-    <h1>Hello World</h1>
-  );
-}
+const Home = () => {
+  return <h1 className="text-3xl text-white font-black">Hello World</h1>;
+};
 
 export default Home;
