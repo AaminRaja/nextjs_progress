24dcc7752c5335441c3a7964f4fd36e8fc3014cf
Aamin Raja <be.intern02@appadore.com>
2024-12-23 11:20:46 +0530

    Tailwind CSS Setup & Theaming

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 const Home = () => {
-  return <h1 className="text-3xl text-white font-black">Hello World</h1>;
+  return (
+    // <h1 className="text-3xl font-black text-primary-500 bg-dark-100/[.8] hover:bg-black">
+    //   Hello World
+    // </h1>
+
+    <h1 className="">Welcom to the World of next.js</h1>
+  );
 };
 
 export default Home;
